4c3862749634e39f0eea33a2b73c3876c88e40ff
Dave Mateer <davemateer@gmail.com>
2022-03-10 10:58:23 +0000

    a

--- image-download-test/Pages/ImageTest.cshtml.cs
+++ image-download-test/Pages/ImageTest.cshtml.cs
@@ -2,29 +2,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ImageDownloadTest.Pages
+namespace ImageDownloadTest.Pages;
+
+public class ImageModel : PageModel
 {
-    public class ImageModel : PageModel
+    // jpg
+    //public IActionResult OnGet()
+    //{
+    //    var fileName = @"C:\dev\test\image-download-test\image-download-test\secret\image.jpg";
+
+    //    // 3. opens chrome image viewer 
+    //    FileStream stream = new FileStream(fileName, FileMode.Open);
+    //    return new FileStreamResult(stream, MediaTypeNames.Image.Jpeg);
+    //    //return new FileStreamResult(stream, "image/jpeg");
+    //}
+
+    // png
+    public IActionResult OnGet()
     {
-        // jpg
-        //public IActionResult OnGet()
-        //{
-        //    var fileName = @"C:\dev\test\image-download-test\image-download-test\secret\image.jpg";
+        var fileName = @"C:\dev\test\image-download-test\image-download-test\secret\image.png";
 
-        //    // 3. opens chrome image viewer 
-        //    FileStream stream = new FileStream(fileName, FileMode.Open);
-        //    return new FileStreamResult(stream, MediaTypeNames.Image.Jpeg);
-        //    //return new FileStreamResult(stream, "image/jpeg");
-        //}
-
-        // png
-        public IActionResult OnGet()
-        {
-            var fileName = @"C:\dev\test\image-download-test\image-download-test\secret\image.png";
-
-            // 3. opens chrome image viewer 
-            FileStream stream = new FileStream(fileName, FileMode.Open);
-            return new FileStreamResult(stream, "image/png");
-        }
+        // 3. opens chrome image viewer 
+        FileStream stream = new FileStream(fileName, FileMode.Open);
+        return new FileStreamResult(stream, "image/png");
     }
 }
--- image-download-test/Pages/PdfTest.cshtml.cs
+++ image-download-test/Pages/PdfTest.cshtml.cs
@@ -1,32 +1,30 @@
-﻿using System.Net.Mime;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ImageDownloadTest.Pages
+namespace ImageDownloadTest.Pages;
+
+public class PdfModel : PageModel
 {
-    public class PdfModel : PageModel
+
+    public IActionResult OnGet()
     {
+        // virtual path (our file is outside of the virtual path so can't use this)
+        //return File("/secret/sample.pdf", "application/pdf", "foo.pdf");
 
-        public IActionResult OnGet()
-        {
-            // virtual path (our file is outside of the virtual path so can't use this)
-            //return File("/secret/sample.pdf", "application/pdf", "foo.pdf");
+        var fileName = @"C:\dev\test\image-download-test\image-download-test\secret\sample.pdf";
 
-            var fileName = @"C:\dev\test\image-download-test\image-download-test\secret\sample.pdf";
+        // 1. filestream - works as a download
+        //FileStream stream = new FileStream(fileName, FileMode.Open);
+        //return File(stream, "application/pdf", "foo.pdf");
 
-            // 1. filestream - works as a download
-            //FileStream stream = new FileStream(fileName, FileMode.Open);
-            //return File(stream, "application/pdf", "foo.pdf");
+        // 2. bytearray - works as a download
+        //var bytes = System.IO.File.ReadAllBytes(fileName);
+        //return File(bytes, "application/pdf", "foo.pdf");
+        // notice constant for application types
+        //return File(bytes, MediaTypeNames.Application.Pdf, "foo.pdf");
 
-            // 2. bytearray - works as a download
-            //var bytes = System.IO.File.ReadAllBytes(fileName);
-            //return File(bytes, "application/pdf", "foo.pdf");
-            // notice constant for application types
-            //return File(bytes, MediaTypeNames.Application.Pdf, "foo.pdf");
-
-            // 3. opens pdf view 
-            FileStream stream = new FileStream(fileName, FileMode.Open);
-            return new FileStreamResult(stream, "application/pdf");
-        }
+        // 3. opens pdf view 
+        FileStream stream = new FileStream(fileName, FileMode.Open);
+        return new FileStreamResult(stream, "application/pdf");
     }
 }
